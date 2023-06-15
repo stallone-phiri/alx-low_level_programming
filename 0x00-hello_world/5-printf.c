d42ff2c3284f857f4ebe8248ed38acce88204a64
stallone-phiri <phiristallonecj@gmail.com>
2023-06-15 15:43:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - (a program that prints a line with printf)
+ * return: (0) (Outcome means the program is successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
