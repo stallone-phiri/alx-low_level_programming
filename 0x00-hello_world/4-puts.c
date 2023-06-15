7039c3889da572321e3bdc38da633e3ba4f90092
stallone-phiri <phiristallonecj@gmail.com>
2023-06-15 14:48:15 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main -  This is a program that prints a line with puts function
+ *
+ * Return: 0 (This signifies that the program is a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
